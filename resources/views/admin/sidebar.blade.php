81e5ec8db6b67ef9aa51d83eab1666b7281a5610
Ozodbek <ozodbekozodov0002@gmail.com>
2022-05-10 13:01:56 +0500

    rooms jadval, model va Controller yaratildi

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,7 +5,7 @@
                 <li class="nav-item {{  request()->routeIs('admin.users.index') ? 'active' : '' }}">
                     <a href="{{route('admin.users.index')}}">
                         <i class="fas fa-user"></i>
-                        <p>Пользователь</p>
+                        <p>Foydalanuvchi</p>
                     </a>
                 </li>
 
