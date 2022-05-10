6564608e1b21a6e2ad2feedeaadf729ca09ca319
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-05-10 23:16:55 +0500

    students barcha funksiyalari ishladi.

--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -4,7 +4,7 @@
         <div class="card">
             <div class="card-header">
                 <div class="row">
-                    <div class="col-9"><h1 class="card-title">Yangiliklar</h1></div>
+                    <div class="col-9"><h1 class="card-title">Talabalar</h1></div>
                     <div class="col-md-1">
                         <a class="btn btn-primary" href="{{route('admin.students.create')}}">
                             <span class="btn-label">
@@ -19,31 +19,31 @@
                     <table class="table table-bordered">
                         <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Ism</th>
-                            <th scope="col">Familya</th>
-                            <th scope="col">Telefon raqam</th>
+                            <th class="" scope="col">#</th>
+                            <th class="" scope="col">Ism</th>
+                            <th class="" scope="col">Familya</th>
+                            <th  class="w-25" scope="col">Telefon raqam</th>
 
-                            <th scope="col">Harakat</th>
+                            <th class="w-25" scope="col">Harakat</th>
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($posts as $post)
+                        @foreach($posts as $ind=>$post)
                             <tr>
-                                <th scope="row" class="col-1">{{$post->id}}</th>
+                                <th scope="row" class="col-1">{{$ind+1}}</th>
                                 <td>{{$post->name}}</td>
                                 <td>{{$post->surname}}</td>
                                 <td>{{$post->phone}}</td>
 
                                 <td class="col-2">
-                                    <form action="" method="POST">
-                                        <a class="btn btn-primary btn-sm" href="">
+                                    <form action="{{route('admin.students.destroy',$post->id)}}" method="POST">
+                                        <a class="btn btn-primary btn-sm" href="{{route('admin.students.show',$post->id)}}">
                                     <span class="btn-label">
                                         <i class="fa fa-eye"></i>
                                     </span>
 
                                         </a>
-                                        <a class="btn btn-warning btn-sm" href="">
+                                        <a class="btn btn-warning btn-sm" href="{{route('admin.students.edit',$post->id)}}">
                                     <span class="btn-label">
                                         <i class="fa fa-pen"></i>
                                     </span>
@@ -58,35 +58,6 @@
                                 </td>
                             </tr>
                         @endforeach
-<tr>
-    <th scope="row" class="col-1">id</th>
-    <td>Birnars</td>
-    <td>Yana</td>
-    <td>Nimadir</td>
-
-    <td class="col-2">
-        <form action="" method="POST">
-            <a class="btn btn-primary btn-sm" href="">
-                                    <span class="btn-label">
-                                        <i class="fa fa-eye"></i>
-                                    </span>
-
-            </a>
-
-            <a class="btn btn-warning btn-sm" href="">
-                                    <span class="btn-label">
-                                        <i class="fa fa-pen"></i>
-                                    </span>
-
-            </a>
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger btn-sm"><span class="btn-label">
-                                        <i class="fa fa-trash"></i>
-                                    </span></button>
-        </form>
-    </td>
-</tr>
                         </tbody>
                     </table>
                 </div>
