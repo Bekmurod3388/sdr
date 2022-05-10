be772ed21cabb796afdaed5872f059fd02bdf8cc
Fayzulla8387 <xojayevfayzulla7@gmail.com>
2022-05-10 17:08:01 +0500

    Fakultetni Crud atib bo'ldim

---
+++ resources/views/admin/studentinfo/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.master')
+@section('content')
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-10"><h1 class="card-title">Yangiliklarni yangilash</h1></div>
+                </div>
+                <hr>
+                <div class="card-body">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+
+                    <form action="{{route('admin.student_info.update',$post)}}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="header_ru">ID</label>
+                            <input type="text" name="id" class="form-control" id="header_ru" placeholder="Титул" value="{{$post->id}}">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="description_ru">Fakultet</label>
+                            <input type="text" name="name" class="form-control" id="header_ru" placeholder="Титул" value="{{$post->name}}">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Saqlash</button>
+                        <input type="reset" class="btn btn-danger" value="Очистить">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
