f2a5fb02af9bee408043b4748ee074ea7fffe72d
bekmurod <bekmurod3388@gmail.com>
2022-05-11 15:38:15 +0500

    Floor request added

--- app/Http/Controllers/FloorController.php
+++ app/Http/Controllers/FloorController.php
@@ -9,9 +9,6 @@ use Illuminate\Http\Request;
 class FloorController extends Controller
 {
 
-    public function __construct(){
-        $this->middleware('auth');
-    }
 
     public function index()
     {
