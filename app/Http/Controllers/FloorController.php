81940ae761f2cccb50102483d6faa9b4b788340e
Ozodbek <ozodbekozodov0002@gmail.com>
2022-05-11 14:29:43 +0500

    qavatni ham yaratdm

---
+++ app/Http/Controllers/FloorController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Floor;
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class FloorController extends Controller
+{
+    public function index()
+    {
+
+        $data = Floor::orderBy('floor', 'ASC')->paginate(5);
+
+        return view('admin.floors.floor',compact('data'));
+    }
+
+    public function create(){
+
+        return view('admin.floors.addfloor');
+
+    }
+
+
+    public function store(Request $request)
+    {
+
+        $data = new Floor();
+        $data->floor = $request->floor;
+        $data->save();
+
+        return redirect(route('admin.floors.index'));
+    }
+
+    public function edit($id)
+    {
+
+        $data = Floor::find($id);
+        return view('admin.floors.editfloor',compact('data'));
+
+    }
+
+
+    public function update(Request $request, $id)
+    {
+
+        $data = Floor::find($id);
+        $data->floor = $request->floor;
+        $data->save();
+
+        return redirect(route('admin.floors.index'));
+
+
+    }
+
+    public function destroy($id)
+    {
+
+        $data = Floor::find($id);
+        $data->delete();
+
+        return redirect(route('admin.floors.index'));
+
+    }
+
+
+}
