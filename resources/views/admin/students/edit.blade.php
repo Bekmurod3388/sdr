3f00746d6cacdcb90bc629d8466e6607b0423872
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-05-11 23:08:07 +0500

    student error messasge in uzbek

--- resources/views/admin/students/edit.blade.php
+++ resources/views/admin/students/edit.blade.php
@@ -11,7 +11,7 @@
 
                     @if ($errors->any())
                         <div class="alert alert-danger">
-                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <strong>Xatolik!</strong> Quyidagi shartlar bajarilishi lozim !<br><br>
                             <ul>
                                 @foreach ($errors->all() as $error)
                                     <li>{{ $error }}</li>
