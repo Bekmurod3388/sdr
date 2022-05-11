4484f3067f23a01555fb22be567a2c03406671b2
Ozodbek <ozodbekozodov0002@gmail.com>
2022-05-11 11:14:54 +0500

    xona bn fakultet qoshishni taxladm

--- resources/views/admin/facultets/create.blade.php
+++ resources/views/admin/facultets/create.blade.php
@@ -4,7 +4,7 @@
         <div class="card">
             <div class="card-header">
                 <div class="row">
-                    <div class="col-10"><h1 class="card-title">Yangilik qo'shing</h1></div>
+                    <div class="col-10"><h1 class="card-title">Fakultet qo'shing</h1></div>
                 </div>
                 <hr>
                 <div class="card-body">
